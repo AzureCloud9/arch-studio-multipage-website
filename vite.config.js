7eb0b82c8810ce1d1bdf9a0b7c2f42ecb7fdce74
MarleyM1999 <marleyalmere@gmail.com>
2023-07-02 19:40:33 +0200

    change

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
-import { defineConfig } from 'vite'  
-import react from '@vitejs/plugin-react'  
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/  
-export default defineConfig(({ command }) => {  
-  const config = {  
-    plugins: [react()],  
-    base: '/',  
-  }  
-
-  // Set base to the repository name when not serving locally
-  if (command !== 'serve') {  
-    config.base = '/arch-studio-multipage-website/'  
-  }  
-
-  return config  
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/arch-studio-multipage-website/"
 })
