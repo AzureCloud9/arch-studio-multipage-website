5bfc51959222ecd90a1df0d8c7e991d3c481fcae
MarleyM1999 <marleyalmere@gmail.com>
2023-07-02 21:27:51 +0200

    lo

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,20 @@
-import { createBrowserRouter,  Route, createRoutesFromElements, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from "react-router-dom";
+import RootLayout from "./layout/RootLayout";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
-//Layout
-import RootLayout from "./layout/RootLayout"
-
-//pages
-import Home from "./pages/Home"
-import NotFound from "./pages/NotFound"
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout/>}>
+const router = createBrowserRouter({
+  basename: '/arch-studio-multipage-website',
+  routes: createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
-      <Route path="*" element={<NotFound/>}/>
-
-
+      <Route path="*" element={<NotFound />} />
     </Route>
   )
-)
-
-
-
-
+});
 
 function App() {
-
-  return (
-    <RouterProvider router={router}/>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
