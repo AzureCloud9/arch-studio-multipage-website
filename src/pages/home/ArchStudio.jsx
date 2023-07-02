2fe07690f2725e5c911a0089c546664e1d94eb67
MarleyM1999 <marleyalmere@gmail.com>
2023-07-03 01:52:16 +0200

    added another section

---
+++ src/pages/home/ArchStudio.jsx
@@ -0,0 +1,14 @@
+import "./ArchStudio.css"
+
+export default function ArchStudio() {
+  return (
+    <div className="archstudio">
+        <h2>Welcome to <br></br> Arch Studio</h2>
+        <p>We have a unique network and skillset to <br></br> help bring your projects to life. Our small <br></br> team of highly skilled individuals <br></br> combined with our large network put us <br></br>in a strong position to deliver <br></br> exceptional results.<br></br>
+
+        Over the past 10 years, we have worked <br></br> on all kinds of projects. From stations to <br></br> high-rise buildings, we create spaces <br></br>that inspire and delight. <br></br>
+        
+        We work closely with our clients so that <br></br> we understand the intricacies of each <br></br> project. This allows us to work in <br></br> harmony the surrounding area to create <br></br>truly stunning projects that will stand the <br></br> test of time.</p>
+    </div>
+  )
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
 import './Home.css'
 import Hero from './Hero'
+import ArchStudio from './ArchStudio'
 
 
 export default function Home() {
   return (
-    <Hero />
+    <>
+      <Hero />
+      <ArchStudio/>
+    </>
   )
 }
