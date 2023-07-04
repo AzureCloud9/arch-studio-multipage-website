1b7ac3c9e2e3d023248e3b20af0cc6b0fb3e46e5
MarleyM1999 <marleyalmere@gmail.com>
2023-07-04 11:30:54 +0200

    wrapper bug
    bugged the page...

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
 import { createBrowserRouter,  Route, createRoutesFromElements, RouterProvider } from "react-router-dom"
-import { useLayoutEffect, useLocation } from 'react'
 
-const Wrapper = ({children}) => {
-  const location = useLocation();
-  useLayoutEffect(() => {
-    document.documentElement.scrollTo(0, 0);
-  }, [location.pathname]);
-  return children
-} 
+
 
 //Layout
 import RootLayout from "./layout/RootLayout"
@@ -19,13 +12,11 @@ import Portfolio from './pages/portfolio/Portfolio'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Wrapper>
       <Route path='/' element={<RootLayout />}>
         <Route index element={<Home />} />
         <Route path='/portfolio' element={<Portfolio/>}/>
         <Route path='*' element={<NotFound />} />
       </Route>
-    </Wrapper>
 
   ),
   {
